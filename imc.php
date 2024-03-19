52d9c649c89a756819fbb84a21bc743b63212e8a
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2024-03-19 19:25:40 -0300

    .
    .

--- imc.php
+++ imc.php
@@ -13,14 +13,23 @@
         <h1>Calculadora IMC</h1>
         <form method="get" action="">
             <div class="mb-3">
-                <label for="formGroupExampleInput" class="form-label">Example label</label>
-                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
+                <label for="formGroupExampleInput" class="form-label">Peso</label>
+                <input type="text" class="form-control" name="peso" id="formGroupExampleInput" placeholder="Digite seu Peso">
             </div>
             <div class="mb-3">
-                <label for="formGroupExampleInput2" class="form-label">Another label</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                <label for="formGroupExampleInput2" class="form-label">Altura</label>
+                <input type="text" class="form-control"  name="altura" id="formGroupExampleInput2" placeholder="Digite sua Altura">
+            </div>
+            <div class="mb-3">
+                <button type="submit">Salvar</button>
             </div>
         </form>
+        <?php
+            if(!empty($_GET)){
+                $imc = $_GET['peso']/($_GET['altura']*$_GET['altura']);
+                echo "<h2>SEU IMC É DE ".$imc." </h2>";
+            }
+        ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
