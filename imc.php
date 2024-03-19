cc9d997ec00db99189bc0b3efabc301d52f7e467
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2024-03-19 19:58:25 -0300

    .
    .

--- imc.php
+++ imc.php
@@ -18,17 +18,27 @@
             </div>
             <div class="mb-3">
                 <label for="formGroupExampleInput2" class="form-label">Altura</label>
-                <input type="text" class="form-control"  name="altura" id="formGroupExampleInput2" placeholder="Digite sua Altura">
+                <input type="text" class="form-control" name="altura" id="formGroupExampleInput2" placeholder="Digite sua Altura">
             </div>
             <div class="mb-3">
                 <button type="submit" class="btn btn-dark">Salvar</button>
             </div>
         </form>
         <?php
-            if(!empty($_GET)){
-                $imc = $_GET['peso']/($_GET['altura']*$_GET['altura']);
-                echo "<h2>SEU IMC É DE ".$imc." </h2>";
+        if (!empty($_GET)) {
+            $imc = round($_GET['peso'] / ($_GET['altura'] * $_GET['altura']),2);
+            if ($imc < 18.5) {
+                echo "<h2>SEU IMC É DE " . $imc . " e você está seco de doer. </h2>";
+            } elseif ($imc >= 18.5 and $imc <= 24.9) {
+                echo "<h2>SEU IMC É DE " . $imc . " e você está normal. </h2>";
+            } elseif ($imc >= 25 and $imc <= 29.9) {
+                echo "<h2>SEU IMC É DE " . $imc . " e você está com sobrepeso e com obesidade grau 1. </h2>";
+            } elseif ($imc >= 30 and $imc <= 39.9) {
+                echo "<h2>SEU IMC É DE " . $imc . " e você está com sobrepeso e com obesidade grau 2. </h2>";
+            } else {
+                echo "<h2>SEU IMC É DE " . $imc . " e você está com muito sobrepeso (quase não anda) e com obesidade grau 3. </h2>";
             }
+        }
         ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
